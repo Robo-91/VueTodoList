044f7f746a95c40b985278f961f76e5c2af092da
Robert Palmer <r91palmer@gmail.com>
2020-09-02 12:01:50 -0500

    added styling and checkbox for each todo

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto auto auto;
+  width: 35em;
+}
+button {
+  cursor: pointer;
+  outline: none;
+}
+.new-todo {
+  text-align: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  box-shadow: 0px 0px 7px 0px rgba(102,102,102,0.75);
+  display: block;
+  margin: 3em auto 1em auto;
+  border: none;
+  border-radius: 10px;
+  width: 23em;
+  height: 2.5em;
+  outline: none;
 }
 </style>
--- src/components/todo.vue
+++ src/components/todo.vue
@@ -2,13 +2,13 @@
   <div id="todolist">
     <ul>
       <li v-for="(todo, index) in todos"
-          :key="index">{{ todo.info }}
-          <button @click="removeTodo(index)">X</button>
+          :key="index"><input type='checkbox'>{{ todo.info }}
+          <button class='delete' @click="removeTodo(index)">X</button>
       </li>
       
     </ul>
-    <input type="text" v-model='newTodo' placeholder="Add Todo Here">
-    <button type="submit" @click="addTodo">Add Todo</button>
+    <input class='new-todo' type="text" v-model='newTodo' placeholder="Add Todo Here">
+    <button class='add' type="submit" @click="addTodo">Add Todo</button>
   </div>
 </template>
 
@@ -45,12 +45,45 @@ export default {
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   div {
     text-align: center;
   }
+  ul {
+    padding-inline-start: 0;
+  }
   li {
     list-style: none;
+    padding: 1.3em;
+    display: flex;
+    justify-content: space-between;
+    margin: 1.5em 1em 1.5em 1em;
+    box-shadow: 0px 0px 20px -7px rgba(69,69,69,0.8);
+    border-radius: 10px;
+  }
+  .delete {
+    background-color: crimson;
+    border: none;
+    border-radius: 50%;
+    color: white;
+    font-weight: bolder;
+    width: 22px;
+  }
+  .delete:hover {
+    background-color:lightcoral;
+    transition: .2s;
+  }
+  .add {
+    margin: 1em;
+    color: white;
+    background-color: #5bc0de;
+    border: none;
+    border-radius: 10px;
+    width: 12em;
+    height: 2em;
+    font-weight: bolder;
+  }
+  .add:hover {
+    box-shadow: inset 0px 0px 50px -27px rgba(0,0,0,1);
   }
 </style>
