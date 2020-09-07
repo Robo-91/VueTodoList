97a57e86763b8be099dc4bbd98b14db4a66edb6b
Robert Palmer <r91palmer@gmail.com>
2020-09-07 11:59:11 -0500

    addes styles to heading

--- src/components/heading.vue
+++ src/components/heading.vue
@@ -13,5 +13,11 @@ export default {
 </script>
 
 <style scoped>
-
+    h1 {
+        padding: 1em 10px 1em 10px;
+        border-radius: 10px;
+        background-color: #5bc0de;
+        color: white;
+        margin: 1.5em 10px 1.5em 10px;
+    }
 </style>
